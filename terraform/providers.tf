1b0164f8659470bbc013f63f2a12357d70ce3fc4
Yamile Centa <yamilecenta0519@gmail.com>
2026-05-06 15:44:53 -0500

    feat: actualizar proveedores

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,21 +1,13 @@
 terraform {
-    required_version = ">= 1.0.0"
-
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
-    default_tags {
-        tags = {
-            Project     = "ImageProcessor"
-            Environment = var.environment
-            Owner       = "ycentafdev"
-        }
-    }
+  region = var.aws_region
+  profile = "ycentafdev"
 }
